3404a3e0d2060e50e4a19e5605e16e4cbe00e363
Lqhuy <lequanghuy1252206@gmail.com>
2023-09-06 15:13:03 +0700

    update ex1

--- 11_2Bai/Day_Count.c
+++ 11_2Bai/Day_Count.c
@@ -21,9 +21,6 @@ typedef enum dayofweek{
 } DayofWeek;
 
 Date date = {3, 1, 2000}; // Thứ 2
-
-Date test = {7, 1, 2000};
-
 Date dateCurrent = {6, 9, 2023};
 
 int isLeapYear(uint16_t year);
@@ -32,26 +29,21 @@ int DayinMonth(uint8_t month, uint16_t year);
 int main(int argc, char const *argv[])
 {
     uint16_t count=0;
-
     while(date.day != dateCurrent.day || date.month != dateCurrent.month || date.year != dateCurrent.year){
-
         count++;
         date.day++;
 
         if(date.day > DayinMonth(date.month, date.year)){
             date.day = 1;
             date.month++;
-
             if(date.month > 12){
                 date.month=1;
                 date.year++;
             }
         }
     }
-
     char *dayName[] = {"T2", "T3", "T4", "T5", "T6", "T7", "CN"};
     printf("%d: %s", count, dayName[count%7]);
-     
     return 0;
 }
 /* 
@@ -65,7 +57,6 @@ int DayinMonth(uint8_t month, uint16_t year){
 
     return DayinMonth[month];
 }
-
 /* 
  *      @brief Check Leap Year 
  */
